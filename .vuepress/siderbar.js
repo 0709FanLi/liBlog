ec042c2b8d900b6913a2c945a98353e436384be6
20005117 <liguangyuan@mychery.com>
2022-03-07 19:02:05 +0800

    掘金整理

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -5,9 +5,13 @@ const BROWSER_PATH = '/blogs/browser'
 module.exports = {
   [JAVASCRIPT_PATH]: [
     createSideBarConfig('JS-基础', JAVASCRIPT_PATH + '/base'),
-    createSideBarConfig('JS-手写', BROWSER_PATHBROWSER_PATH + '/handwritten'),
+    createSideBarConfig('JS-手写', JAVASCRIPT_PATH + '/handwritten'),
   ],
-  BROWSER_PATH: [
-    createSideBarConfig('浏览器', BROWSER_PATH),
+  [BROWSER_PATH]: [
+    createSideBarConfig('网络', BROWSER_PATH, + '/net'),
+    //  compatible  兼容性
+    createSideBarConfig('安全', BROWSER_PATH, + '/safe'),
+    createSideBarConfig('渲染', BROWSER_PATH, + '/render'),
+    createSideBarConfig('兼容性', BROWSER_PATH, + '/compatible'),
   ],
 }
