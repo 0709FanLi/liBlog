e1e31c12c1716c29cf9b80b15fd0632d3431f201
20005117 <liguangyuan@mychery.com>
2022-03-07 18:17:51 +0800

    browser

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,9 +1,13 @@
 const { createSideBarConfig } = require('./util')
 const JAVASCRIPT_PATH = '/blogs/javascript'
+const BROWSER_PATH = '/blogs/browser'
 
 module.exports = {
   [JAVASCRIPT_PATH]: [
     createSideBarConfig('JS-基础', JAVASCRIPT_PATH + '/base'),
-    createSideBarConfig('JS-手写', JAVASCRIPT_PATH + '/handwritten'),
-  ]
+    createSideBarConfig('JS-手写', BROWSER_PATHBROWSER_PATH + '/handwritten'),
+  ],
+  BROWSER_PATH: [
+    createSideBarConfig('浏览器', BROWSER_PATH),
+  ],
 }
